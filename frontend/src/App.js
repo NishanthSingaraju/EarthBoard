527e1f53e0e534a8a0b1d5fb139d758409589e2a
Nishanth Singaraju <nsingaraju@google.com>
2022-02-19 20:10:29 -0500

    added polygons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,11 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Dashboard from './Dashboard';
+import MLView from "./MLView"
 
 function App() {
     return (
-      <Dashboard></Dashboard>
+      <MLView></MLView>
     );
   }
   
